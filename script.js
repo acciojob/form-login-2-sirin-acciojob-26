6a5a99314d454f9b5ad3ac36e8c1b39a8b65adc7
sirin-acciojob-26 <166235930+sirin-acciojob-26@users.noreply.github.com>
2024-08-05 15:57:44 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your JS code here. If required.
+     function showAlert() {
+            var firstName = document.getElementById("firstName").value;
+            var lastName = document.getElementById("lastName").value;
+            var phoneNumber = document.getElementById("phoneNumber").value;
+            var emailID = document.getElementById("emailID").value;
+
+            alert("First Name: " + firstName + " Last Name: " + lastName + " Phone Number: " + phoneNumber + " Email ID: " + emailID);
+        }
